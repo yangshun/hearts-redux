9e6f45754756aa9956a9a1504138cbd4899b9ed1
Tay Yang Shun <tay.yang.shun@gmail.com>
2017-03-05 01:58:55 +0800

    publish: Add deploy.sh
    generated from commit bbb20e319dae699e2aa72e3c4bd609b438c9d330

--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom';
-import routes from 'routes';
-
-import 'main.scss';
-
-ReactDOM.render(routes(), document.getElementById('app'));
--- src/js/types/redux.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-// Flux Standard Action: https://github.com/acdlite/flux-standard-action
-export type FSA = {
-  type: string,
-  payload: any,
-  meta?: any,
-  error?: boolean,
-};
